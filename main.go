ad9aa7a28b4a01a40a413758542f919e6dad9ae9
Professor Caio <caio.silva@ceub.edu.br>
2024-05-15 10:33:02 +0000

    feat: initial version of insert user no working without validations

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import ("net/http"
+		"log"
+		"encoding/json"
+		"io"
+		
+		"go.mongodb.org/mongo-driver/mongo"
+		"go.mongodb.org/mongo-driver/bson/primitive")
 
 func main() {
 
@@ -12,9 +18,60 @@ func Run() {
 	
 	s := http.Server{
 		Addr: "localhost:8080",
+		Handler: Router{},
 	} 
 	
 	s.ListenAndServe()
 }
+
+
+type Router struct{}
+
+
+func (Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	
+	var u User
+	body, _ := io.ReadAll(req.Body)
+	json.Unmarshal(body, &u)
+	Insert(u, "users")
+}
+
+
+type User struct {
+	Name string `json:"name" bson:"name"`
+	RA string `json:"ra" bson:"ra"`
+	Email string `json:"email" bson:"email"`
+	Pass string `json:"pass" bson:"email"`
+}
+
+
+func Insert(u any, collection string) (primitive.ObjectId, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	
+	defer cancel()
+	
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	
+	collection := client.Database("nome-do-banco").Collection(collection)
+	
+	res, err := collection.InsertOne(ctx, u)
+	
+	return res.InsertedID, nil 
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
